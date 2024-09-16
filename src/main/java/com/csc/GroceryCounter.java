8165bc8671bab992d4c9decef0c66ad895f17ee8
dakotawhite-code <wd53169@mail.harpercollege.edu>
2024-09-15 22:34:58 -0500

    done

--- src/main/java/com/csc/GroceryCounter.java
+++ src/main/java/com/csc/GroceryCounter.java
@@ -1,5 +1,96 @@
 package com.csc;
-
 public class GroceryCounter {
-  // Put your code here!
+    int count;
+    int overflowcount;
+
+    public GroceryCounter() {
+        this.count = 0;
+        this.overflowcount = 0;
+    }
+
+    public void tens() {
+      count += 1000;
+      checkoverflow();
+  }
+
+    public void ones() {
+        count += 100;
+        checkoverflow();
+    }
+
+    public void tenths() {
+        count += 10;
+        checkoverflow();
+    }
+
+    public void hundreths() {
+        count += 1;
+        checkoverflow();
+    }
+
+    void checkoverflow() {
+        if (count > 9999) {
+            count = count % 10000;
+            overflowcount++;
+        }
+    }
+
+    public String total() {
+        int dollars = count / 100;
+        int cents = count % 100;
+        String formatdollars = String.format("%02d", dollars);
+        String formatcents = String.format("%02d", cents);
+        return "$" + formatdollars + "." + formatcents;
+    }
+
+    public int number_of_overflows() {
+        return overflowcount;
+    }
+
+    public void clear() {
+        this.count = 0;
+        this.overflowcount = 0;
+    }
+
+    public static void main(String[] args) {
+        GroceryCounter counter = new GroceryCounter();
+        System.out.println("Welcome to the Grocery Counter. Count'em Up!\n" + counter.total());
+        
+        counter.tens();
+        System.out.println(counter.total());
+       
+        for (int i = 0; i < 2; i++) {
+          counter.ones();
+          }
+          System.out.println(counter.total());
+
+        for (int i = 0; i < 3; i++) {
+          counter.tenths();
+          }
+        System.out.println(counter.total());
+        
+        for (int i = 0; i < 4; i++) {
+          counter.hundreths();
+        }
+        System.out.println(counter.total());
+        System.out.println("Amount spent on groceries: " + counter.total());
+
+        for (int i = 0; i < 10000; i++) {
+            counter.hundreths();
+        }
+        System.out.println("Number of overflows: " + counter.number_of_overflows());
+      
+        counter.clear();
+        System.out.println("\nReset Counter\n" + counter.total());
+        System.out.println("Number of overflows: " + counter.number_of_overflows());
+    
+        for (int i = 0; i < 5; i++) {
+          counter.ones();
+          }
+        for (int i = 0; i < 9; i++) {
+              counter.hundreths();
+              }
+        System.out.println("\nAmount spent on groceries: " + counter.total());
+        System.out.println("Number of overflows: " + counter.number_of_overflows());
+      }
 }
--- src/test/java/com/csc/TestGroceryCounter.java
+++ src/test/java/com/csc/TestGroceryCounter.java
@@ -1,7 +1,6 @@
 package com.csc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,11 +13,64 @@ public class TestGroceryCounter {
     counter = new GroceryCounter();
   }
 
-  // Put your tests here!
-
+      @Test
+      public void testinitial() {
+          assertEquals("$00.00", counter.total());
+      }
+  
+      @Test
+      public void testtens() {
+          counter.tens();
+          assertEquals("$10.00", counter.total());
+      }
+  
+      @Test
+      public void testones() {
+          counter.ones();
+          assertEquals("$01.00", counter.total());
+      }
+  
+      @Test
+      public void testtenths() {
+          counter.tenths();
+          assertEquals("$00.10", counter.total());
+      }
+  
+      @Test
+      public void testhundreths() {
+          counter.hundreths();
+          assertEquals("$00.01", counter.total());
+      }
+  
+      @Test
+      public void testoverflow() {
+          for (int i = 0; i < 10000; i++) {
+              counter.hundreths();
+          }
+          assertEquals("$00.00", counter.total());
+          assertEquals(1, counter.number_of_overflows());
+      }
+  
+      @Test
+      public void testclear() {
+          counter.tens();
+          counter.ones();
+          counter.clear();
+          assertEquals("$00.00", counter.total());
+          assertEquals(0, counter.number_of_overflows());
+      }
+  
+      @Test
+      public void testmany() {
+          counter.ones();
+          counter.ones();
+          counter.tenths();
+          counter.hundreths();
+          assertEquals("$02.11", counter.total());
+      }
+  
   @Test
   void itWorks() {
     assertEquals(true, true);
   }
-
 }
